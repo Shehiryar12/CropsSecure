29b69f7768dcb6b107fcb58436bea134e3d4c23d
Shehiryar12 <shehiryar.ranglerz@gmail.com>
2025-08-18 17:02:09 +0500

    git push

--- Src/Components/ReceiptComponents.js
+++ Src/Components/ReceiptComponents.js
@@ -9,41 +9,41 @@ const ReceiptComponents = props => {
         <Text style={styles.status}>{props?.title}</Text>
 
         <View style={styles.header}>
-        <Text style={{color:"#86BC41",fontSize:14,}}>Product</Text>
-        <Text style={{color:"#86BC41",fontSize:14,marginLeft:110,}}>Items</Text>
-        <Text style={{color:"#86BC41",fontSize:14,}}>Price (Rs)</Text>
+        <Text style={{color:"#86BC41",fontSize:14,}}>{props?.productname}</Text>
+        <Text style={{color:"#86BC41",fontSize:14,marginLeft:110,}}>{props?.itemname}</Text>
+        <Text style={{color:"#86BC41",fontSize:14,}}>{props?.price}</Text>
       </View>
 
         <View style={styles.header}>
-        <Text style={{color:"#DBD8D8",fontSize:10,width:160,fontFamily:Font.medium}}>Stick Class Dispersing and adhesive</Text>
-        <Text style={{fontSize:10,color:"#FFFFFF",width:45,fontFamily:Font.medium}}>2</Text>
-        <Text style={{fontSize:10,color:"#FFFFFF",fontFamily:Font.medium,}}>5600</Text>
+        <Text style={{color:"#DBD8D8",fontSize:10,width:160,fontFamily:Font.medium}}>{props?.descriptiontext}</Text>
+        <Text style={{fontSize:10,color:"#FFFFFF",width:45,fontFamily:Font.medium}}>{props?.number}</Text>
+        <Text style={{fontSize:10,color:"#FFFFFF",fontFamily:Font.medium,}}>{props?.pricenumber}</Text>
       </View>
 
        <View style={styles.header}>
-        <Text style={{color:"#DBD8D8",fontSize:10,width:150,fontFamily:Font.medium}}>Strong plus High phosphor that increase</Text>
-        <Text style={{fontSize:10,color:"#FFFFFF",width:37,fontFamily:Font.medium}}>2</Text>
-        <Text style={{fontSize:10,color:"#FFFFFF",fontFamily:Font.medium,}}>5200</Text>
+        <Text style={{color:"#DBD8D8",fontSize:10,width:150,fontFamily:Font.medium}}>{props?.descriptiontexttwo}</Text>
+        <Text style={{fontSize:10,color:"#FFFFFF",width:37,fontFamily:Font.medium}}>{props?.numbertwo}</Text>
+        <Text style={{fontSize:10,color:"#FFFFFF",fontFamily:Font.medium,}}>{props?.pricenumbertwo}</Text>
       </View>
 
         <View style={styles.dottedContainer}></View>
 
          <View style={styles.header}>
-        <Text style={{color:"#DBD8D8",fontSize:10,width:150,fontFamily:Font.medium}}>Total</Text>
-        <Text style={{fontSize:10,color:"#FFFFFF",width:37,fontFamily:Font.medium}}>2</Text>
-        <Text style={{fontSize:10,color:"#FFFFFF",fontFamily:Font.medium,}}>10800</Text>
+        <Text style={{color:"#DBD8D8",fontSize:10,width:150,fontFamily:Font.medium}}>{props?.total}</Text>
+        <Text style={{fontSize:10,color:"#FFFFFF",width:37,fontFamily:Font.medium}}>{props?.totalnumber}</Text>
+        <Text style={{fontSize:10,color:"#FFFFFF",fontFamily:Font.medium,}}>{props?.totalprice}</Text>
       </View>
 
         <View style={styles.dottedContainertwo}></View>
 
         <View style={styles.header}>
-        <Text style={{color:"#DBD8D8",fontSize:10,width:150,fontFamily:Font.medium}}>Claim Amount</Text>
+        <Text style={{color:"#DBD8D8",fontSize:10,width:150,fontFamily:Font.medium}}>{props?.claimamount}</Text>
         {/* <Text style={{fontSize:10,color:"#FFFFFF",width:37,fontFamily:Font.medium}}>2</Text> */}
-        <Text style={{fontSize:10,color:"#FFFFFF",fontFamily:Font.medium,}}>2,000,000</Text>
+        <Text style={{fontSize:10,color:"#FFFFFF",fontFamily:Font.medium,}}>{props?.claimprice}</Text>
       </View>
 
      <View  style={styles.endingContainer}>
-      <Text style={styles.endingtext}>Present this receipt to the dealer to collect the products</Text>
+      <Text style={styles.endingtext}>{props?.endingtext}</Text>
       </View> 
       </View>
 
--- Src/Screens/Receipt.js
+++ Src/Screens/Receipt.js
@@ -1,37 +1,52 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import { Font } from '../Constant/Font';
 import ReceiptComponents from '../Components/ReceiptComponents';
 
 const Receipt = () => {
   return (
-   <SafeAreaView style={styles.container}>
-         <View style={styles.header}>
-           <MaterialIcons name="keyboard-arrow-left" size={23} color="black" />
-           <Text style={styles.heading}>Receipt</Text>
-           <View style={{ width: 36 }}></View>
-         </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <MaterialIcons name="keyboard-arrow-left" size={23} color="black" />
+        <Text style={styles.heading}>Receipt</Text>
+        <View style={{ width: 36 }}></View>
+      </View>
 
-         <ReceiptComponents title="Khan Fertilizer"/>
+      <ReceiptComponents
+        title="Khan Fertilizer"
+        productname="Product"
+        itemname="Items"
+        price="Price (Rs)"
+        descriptiontext="Stick Class Dispersing and adhesive"
+        number={2}
+        pricenumber={5600}
+        descriptiontexttwo="Strong plus High phosphor that increase"
+        numbertwo={2}
+        pricenumbertwo={5200}
+        total="Total"
+        totalnumber={2}
+        totalprice={10800}
+        claimamount="Claim Amount"
+        claimprice={"2,000,000"}
+        endingtext="Present this receipt to the dealer to collect the products"
+      />
+    </SafeAreaView>
+  );
+};
 
-         
-         </SafeAreaView>
-  )
-}
-
-export default Receipt
+export default Receipt;
 
 const styles = StyleSheet.create({
-      header: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        // alignItems: 'center',
-        marginTop: 14,
-        marginLeft: 12,
-      },
-      heading: {
-        fontFamily: Font.bold,
-        fontSize: 15,
-      },
-})
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    // alignItems: 'center',
+    marginTop: 14,
+    marginLeft: 12,
+  },
+  heading: {
+    fontFamily: Font.bold,
+    fontSize: 15,
+  },
+});
